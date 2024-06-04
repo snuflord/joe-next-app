6ecdfe90f09287c633c3fb2aa0f0dab4007b2405
snuflord <joebrennan.work@hotmail.co.uk>
2024-06-04 15:51:19 +0100

    revert throw error and return next response in userloggedin api route

--- src/app/api/getUserToken/route.js
+++ src/app/api/getUserToken/route.js
@@ -1,16 +1,17 @@
 import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers';
+import { cookies } from 'next/headers'
 
 export async function GET(req, res) {
+  try {
+    const nextCookies = cookies()
+    const token = nextCookies.get('token')?.value
+    return NextResponse.json(token)
 
-    try {
-
-        const nextCookies = cookies();
-        const token = nextCookies.get('token')?.value;
-        return NextResponse.json(token)
-
-    } catch (error) {
-        console.log('No user signed in')
-        throw new Error('No user signed in');
-    }
+  } catch (error) {
+    console.log('No user signed in')
+    return new NextResponse(JSON.stringify({ message: 'No user signed in - API getUserToken' }), {
+      status: 401,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
 }
