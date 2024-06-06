1020a36d978587d0c33ba929859a8f517ee26753
snuflord <joebrennan.work@hotmail.co.uk>
2024-06-06 11:44:23 +0100

    new .env files for local and production

--- next.config.mjs
+++ next.config.mjs
@@ -15,19 +15,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//     images: {
-//       domains: ['res.cloudinary.com'],
-//       formats: ['image/avif', 'image/webp',],
-//       remotePatterns: [
-//         {
-//           protocol: 'https',
-//           hostname: 'res.cloudinary.com',
-//           port: '',
-//           pathname: '/image/upload/**',
-//         },
-//       ],
-//     },
-//   }
